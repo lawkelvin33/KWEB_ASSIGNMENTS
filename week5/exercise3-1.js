8c9abaf15b0ca6660a671075f1f4f311dd0bac31
lawkelvin33 <lawkelvin33@korea.ac.kr>
2023-05-16 00:08:21 +0900

    0516

--- week5/exercise3-1.js
+++ week5/exercise3-1.js
@@ -1,18 +1,21 @@
 function isValidNumber(number) {
-  console.log(typeof number === "number");
-  console.log(+number > 0);
-  console.log(+number < 10);
-  console.log(Math.floor(+number) === +number);
-  if (number == +number) return true;
+  if (
+    +number !== NaN &&
+    number > 0 &&
+    number < 10 &&
+    Math.floor(number) === number
+  )
+    return true;
+  return false;
 }
+console.log(+"abc" !== NaN);
 
 console.log(isValidNumber(9));
-console.log("-----------------");
+
 console.log(isValidNumber("4"));
-console.log("-----------------");
+
 console.log(isValidNumber("abc"));
-console.log("-----------------");
+
 console.log(isValidNumber(-5));
-console.log("-----------------");
+
 console.log(isValidNumber(3.5));
-console.log("-----------------");
--- week5/exercise3-2.js
+++ week5/exercise3-2.js
@@ -1,8 +1,12 @@
 function getDivisors(number) {
   const result = [];
-  for (let i = number; i > Math.SQRT(number); i--) {
+  for (let i = number; i > Math.sqrt(number); i--) {
     if (number % i === 0) {
       result.push(i);
     }
   }
+  return result;
 }
+console.log(getDivisors(5));
+console.log(getDivisors(24));
+console.log(getDivisors(196));
