5f82c9d3911b7cf9eeafbc5553590fce02a0f518
lawkelvin33 <lawkelvin33@korea.ac.kr>
2023-05-30 00:16:28 +0900

    수정

--- week6/box-color-btn.js
+++ week6/box-color-btn.js
@@ -1,6 +1,8 @@
 function changeBoxColor() {
-  let box = document.getElementById("box2");
-  box.style.backgroundColor = "blue";
+  let box = document.body.getElementsByClassName("box");
+  box[1].style.backgroundColor = `rgb(${Math.floor(
+    Math.random() * 256
+  )},${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)})`;
 }
 
 /*Code A.8을 참고하여 버튼을 클릭하면 다섯 개 상자 중 두 번째 상자의 색을 변경하는 함수를 box-color-btn.js
